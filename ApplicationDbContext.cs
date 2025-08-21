731467f5c26d94a105ca06a15123b696c7ef11fe
Valeriia <valeriia.vp1@yandex.ru>
2025-08-21 11:22:58 +0300

    Add project files.

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Models;
+
+namespace SocialNetwork
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
