cd034099df2c7a63609958b111b056604fe5fef1
SantinielST <107537475+SantinielST@users.noreply.github.com>
2025-08-21 13:39:52 +0300

    Add files via upload

---
+++ SocialNetwork.BLL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialNetwork.BLL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
