a04eb7c9a6c5ec0febed33db790dfcaa7393000d
SantinielST <107537475+SantinielST@users.noreply.github.com>
2025-08-21 14:01:23 +0300

    Delete SocialNetwor.DLL directory

--- SocialNetwor.DLL/Interfaces/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SocialNetwork.DLL.Interfaces;
-
-public interface IUnitOfWork : IDisposable
-{
-    int SaveChanges(bool ensureAutoHistory = false); //Первый метод — сохранение всех изменений в базу данных (по всем репозиториям).
-
-    IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = true) where TEntity : class; //GetRepository, возвращающий объект
-                                                                                                        //IRepository, — это все репозитории для наших самописных моделей.
-}
