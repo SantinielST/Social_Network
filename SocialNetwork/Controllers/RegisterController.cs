57e6c2ed82fd97f34a774e98a147171ad67301e3
Valeriia <valeriia.vp1@yandex.ru>
2025-08-22 16:37:08 +0300

    First unit back-end logic finished (main page and registration)

---
+++ SocialNetwork/Controllers/RegisterController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.ViewModels;
+using System.Net;
+
+namespace SocialNetwork.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly IMapper _mapper; //преобразует ViewModel -> доменная модель User
+
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public RegisterController(IMapper mapper, UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [Route("Register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("Home/Register");
+        }
+
+        [Route("RegisterPart2")]
+        [HttpGet]
+        public IActionResult RegisterPart2(RegisterViewModel model)
+        {
+            return View("RegisterPart2", model);
+        }
+
+        [Route("Register")]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model) //возврат на вторую форму
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _mapper.Map<User>(model);
+
+                var result = await _userManager.CreateAsync(user, model.PasswordReg);
+            //Автовход: создать аутентификационную cookie
+            //    Второй параметр (isPersistent=false) — сессия до закрытия браузера
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View("RegisterPart2", model);
+        }
+    }
+}
