4ea3a53a181a3317f2364622582f61a9cb8c79cc
Name <email@example.ru>
2025-08-27 09:47:43 +0300

    Restoring commented code

--- SocialNetwork/Controllers/HomeController.cs
+++ SocialNetwork/Controllers/HomeController.cs
@@ -3,33 +3,32 @@ using Microsoft.AspNetCore.Mvc;
 using SocialNetwork.BLL.Models;
 using SocialNetwork.ViewModels;
 
-namespace SocialNetwork.Controllers
+namespace SocialNetwork.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    [Route("")]
+    [Route("[controller]/[action]")]
+    public IActionResult Index()
+    {
+        return View(new MainViewModel());
+    }
 
-        [Route("")]
-        [Route("[controller]/[action]")]
-        public IActionResult Index()
-        {
-            return View(new MainViewModel());
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
--- SocialNetwork/Controllers/RegisterController.cs
+++ SocialNetwork/Controllers/RegisterController.cs
@@ -8,16 +8,16 @@ namespace SocialNetwork.Controllers
 {
     public class RegisterController : Controller
     {
-        //private IMapper _mapper;
+        private IMapper _mapper;
 
-        //private readonly UserManager<User> _userManager;
-        //private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
-        public RegisterController(/*IMapper mapper, UserManager<User> userManager, SignInManager<User> signInManager*/)
+        public RegisterController(IMapper mapper, UserManager<User> userManager, SignInManager<User> signInManager)
         {
-            //_mapper = mapper;
-            //_userManager = userManager;
-            //_signInManager = signInManager;
+            _mapper = mapper;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [Route("Register")]
@@ -34,29 +34,29 @@ namespace SocialNetwork.Controllers
             return View("RegisterPart2", model);
         }
 
-        //[Route("Register")]
-        //[HttpPost]
-        //public async Task<IActionResult> Register(RegisterViewModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var user = _mapper.Map<User>(model);
+        [Route("Register")]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _mapper.Map<User>(model);
 
-        //        var result = await _userManager.CreateAsync(user, model.PasswordReg);
-        //        if (result.Succeeded)
-        //        {
-        //            await _signInManager.SignInAsync(user, false);
-        //            return RedirectToAction("Index", "Home");
-        //        }
-        //        else
-        //        {
-        //            foreach (var error in result.Errors)
-        //            {
-        //                ModelState.AddModelError(string.Empty, error.Description);
-        //            }
-        //        }
-        //    }
-        //    return View("RegisterPart2", model);
-        //}
+                var result = await _userManager.CreateAsync(user, model.PasswordReg);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View("RegisterPart2", model);
+        }
     }
 }
